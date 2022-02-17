2859c8b33d386f027f26a808b2d39ef28007a89b
JWC on iPhone <jwc@jessewonderclark.com>
2022-02-16 22:37:57 -0500

    adjust button placement for iphone

--- ButtonTables.lua
+++ ButtonTables.lua
@@ -1,6 +1,52 @@
-simpleButtons.ui[ [[positioningDemo,positioningDemo,12]] ] = 
-    {text = [[They always remember their positions between launches.]],
-    x = 0.5095168374817, y = 0.56640625,
+simpleButtons.ui[ [[greetingScreen,greetingScreen,29]] ] = 
+    {text = [[next]],
+    x = 0.91032210834553, y = 0.08544921875,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[SimpleButtons,window,42]] ] = 
+    {text = [[button 2]],
+    x = 0.50824587706147, y = 0.39466666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[greetingScreen,greetingScreen,16]] ] = 
+    {text = [[...it can even
+accommodate
+a text block...]],
+    x = 0.74341142020498, y = 0.54345703125,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[greetingScreen,greetingScreen,12]] ] = 
+    {text = [[This is a SimpleButton!]],
+    x = 0.58620689655172, y = 0.89733333333333,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[greetingScreen,greetingScreen,23]] ] = 
+    {text = [[...and you can also manually 
+set the button dimensions 
+if you want.]],
+    x = 0.43853073463268, y = 0.23866666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[positioningDemo,positioningDemo,18]] ] = 
+    {text = [[start]],
+    x = 0.91434846266471, y = 0.08251953125,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[SimpleButtons,window,18]] ] = 
+    {text = [[boo]],
+    x = 0.00099687787290762, y = 0.17746133333333,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[attributesDemo,attributesDemo,27]] ] = 
+    {text = [[Stroke color from stroke().]],
+    x = 0.61769115442279, y = 0.57066666666667,
     action = simpleButtons.defaultButtonAction
 }
 
@@ -10,9 +56,9 @@ simpleButtons.ui[ [[attributesDemo,attributesDemo,37]] ] =
     action = simpleButtons.defaultButtonAction
 }
 
-simpleButtons.ui[ [[attributesDemo,attributesDemo,23]] ] = 
-    {text = [[StrokeWidth from strokeWidth().]],
-    x = 0.74362818590705, y = 0.72933333333333,
+simpleButtons.ui[ [[greetingScreen,greetingScreen,8]] ] = 
+    {text = [[Hi!]],
+    x = 0.26686656671664, y = 0.68666666666667,
     action = simpleButtons.defaultButtonAction
 }
 
@@ -30,86 +76,15 @@ Changing screens can be as simple as typing
     action = simpleButtons.defaultButtonAction
 }
 
-simpleButtons.ui[ [[attributesDemo,attributesDemo,31]] ] = 
-    {text = [[Font from font().]],
-    x = 0.56071964017991, y = 0.46266666666667,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[greetingScreen,greetingScreen,31]] ] = 
-    {text = [[This is a SimpleButton!]],
-    x = 0.58620689655172, y = 0.89733333333333,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[greetingScreen,greetingScreen,27]] ] = 
-    {text = [[Hi!]],
-    x = 0.26686656671664, y = 0.68666666666667,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[greetingScreen,greetingScreen,42]] ] = 
-    {text = [[...and you can also manually 
-set the button dimensions 
-if you want.]],
-    x = 0.43853073463268, y = 0.23866666666667,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[SimpleButtons,window,18]] ] = 
-    {text = [[boo]],
-    x = 0.00099687787290762, y = 0.17746133333333,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[positioningDemo,positioningDemo,14]] ] = 
-    {text = [[You can also manually set x & y if you want,
-but then you can't drag them.]],
-    x = 0.5, y = 0.15,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[actionsDemo,actionsDemo,3]] ] = 
-    {text = [[This button demonstrates button actions (check the output).]],
-    x = 0.51207906295754, y = 0.47705078125,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[greetingScreen,greetingScreen,33]] ] = 
-    {text = [[It sizes itself to fit any text...]],
-    x = 0.66866566716642, y = 0.74666666666667,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[greetingScreen,greetingScreen,48]] ] = 
-    {text = [[next]],
-    x = 0.91032210834553, y = 0.08544921875,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[SimpleButtons,window,42]] ] = 
-    {text = [[button 2]],
-    x = 0.50824587706147, y = 0.39466666666667,
-    action = simpleButtons.defaultButtonAction
-}
-
 simpleButtons.ui[ [[attributesDemo,attributesDemo,42]] ] = 
     {text = [[next]],
     x = 0.91032210834553, y = 0.083984375,
     action = simpleButtons.defaultButtonAction
 }
 
-simpleButtons.ui[ [[SimpleButtons,window,28]] ] = 
-    {text = [[]],
-    x = 0.50824587706147, y = 0.50666666666667,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[greetingScreen,greetingScreen,35]] ] = 
-    {text = [[...it can even
-accommodate
-a text block...]],
-    x = 0.74341142020498, y = 0.54345703125,
+simpleButtons.ui[ [[attributesDemo,attributesDemo,31]] ] = 
+    {text = [[Font from font().]],
+    x = 0.53298350824588, y = 0.43333333333333,
     action = simpleButtons.defaultButtonAction
 }
 
@@ -119,61 +94,6 @@ simpleButtons.ui[ [[attributesDemo,attributesDemo,7]] ] =
 mostly just like Codea's
 other 2D elements.]],
     x = 0.24194729136164, y = 0.7626953125,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[attributesDemo,attributesDemo,27]] ] = 
-    {text = [[Stroke color from stroke().]],
-    x = 0.65742128935532, y = 0.59333333333333,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[attributesDemo,attributesDemo,35]] ] = 
-    {text = [[And font size from fontSize()]],
-    x = 0.46626686656672, y = 0.27733333333333,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[actionsDemo,actionsDemo,6]] ] = 
-    {text = [[next]],
-    x = 0.91032210834553, y = 0.08642578125,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[positioningDemo,positioningDemo,10]] ] = 
-    {text = [[Try it!]],
-    x = 0.48901903367496, y = 0.3603515625,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[positioningDemo,positioningDemo,4]] ] = 
-    {text = [[Positioning is wayyy simple too!
-Just tap "buttons are draggable" 
-in the Parameters panel and
-drag 'em around!]],
-    x = 0.50036603221083, y = 0.83642578125,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[positioningDemo,positioningDemo,4+Positioning is wayyy simple too!
-Just tap "buttons are draggable" 
-and drag 'em!]] ] = 
-    {text = [[Positioning is wayyy simple too!
-Just tap "buttons are draggable" 
-and drag 'em!]],
-    x = 0.47076461769115, y = 0.81866666666667,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[attributesDemo,attributesDemo,39]] ] = 
-    {text = [[(and you can set a custom font color too)]],
-    x = 0.48875562218891, y = 0.097333333333333,
-    action = simpleButtons.defaultButtonAction
-}
-
-simpleButtons.ui[ [[SimpleButtons,window,35]] ] = 
-    {text = [[window title]],
-    x = 0.50824587706147, y = 0.64666666666667,
     action = simpleButtons.defaultButtonAction
 }
 
@@ -187,16 +107,62 @@ Changing screens can be as simple as typing
     action = simpleButtons.defaultButtonAction
 }
 
-simpleButtons.ui[ [[positioningDemo,positioningDemo,18]] ] = 
-    {text = [[start]],
-    x = 0.91434846266471, y = 0.08251953125,
+simpleButtons.ui[ [[SimpleButtons,window,28]] ] = 
+    {text = [[]],
+    x = 0.50824587706147, y = 0.50666666666667,
     action = simpleButtons.defaultButtonAction
 }
 
-simpleButtons.ui[ [[attributesDemo,attributesDemo,17]] ] = 
-    {text = [[They get their fill
-color from fill().]],
-    x = 0.60494752623688, y = 0.89466666666667,
+simpleButtons.ui[ [[actionsDemo,actionsDemo,3]] ] = 
+    {text = [[This button demonstrates button actions (check the output).]],
+    x = 0.51207906295754, y = 0.47705078125,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[attributesDemo,attributesDemo,35]] ] = 
+    {text = [[And font size from fontSize()]],
+    x = 0.46626686656672, y = 0.27733333333333,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[attributesDemo,attributesDemo,23]] ] = 
+    {text = [[StrokeWidth from strokeWidth().]],
+    x = 0.71814092953523, y = 0.71866666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[attributesDemo,attributesDemo,39]] ] = 
+    {text = [[(and you can set a custom font color too)]],
+    x = 0.53073463268366, y = 0.11866666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[positioningDemo,positioningDemo,12]] ] = 
+    {text = [[They always remember their positions between launches.]],
+    x = 0.5095168374817, y = 0.56640625,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[positioningDemo,positioningDemo,14]] ] = 
+    {text = [[You can also manually set x & y if you want,
+but then you can't drag them.]],
+    x = 0.5, y = 0.15,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[positioningDemo,positioningDemo,4+Positioning is wayyy simple too!
+Just tap "buttons are draggable" 
+and drag 'em!]] ] = 
+    {text = [[Positioning is wayyy simple too!
+Just tap "buttons are draggable" 
+and drag 'em!]],
+    x = 0.47076461769115, y = 0.81866666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[actionsDemo,actionsDemo,6]] ] = 
+    {text = [[next]],
+    x = 0.91032210834553, y = 0.08642578125,
     action = simpleButtons.defaultButtonAction
 }
 
@@ -207,6 +173,40 @@ other 2D elements.]] ] =
 just like Codea's
 other 2D elements.]],
     x = 0.27886056971514, y = 0.78266666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[positioningDemo,positioningDemo,10]] ] = 
+    {text = [[Try it!]],
+    x = 0.48901903367496, y = 0.3603515625,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[greetingScreen,greetingScreen,14]] ] = 
+    {text = [[It sizes itself to fit any text...]],
+    x = 0.66866566716642, y = 0.74666666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[positioningDemo,positioningDemo,4]] ] = 
+    {text = [[Positioning is wayyy simple too!
+Just tap "buttons are draggable" 
+in the Parameters panel and
+drag 'em around!]],
+    x = 0.50036603221083, y = 0.83642578125,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[SimpleButtons,window,35]] ] = 
+    {text = [[window title]],
+    x = 0.50824587706147, y = 0.64666666666667,
+    action = simpleButtons.defaultButtonAction
+}
+
+simpleButtons.ui[ [[attributesDemo,attributesDemo,17]] ] = 
+    {text = [[They get their fill
+color from fill().]],
+    x = 0.58020989505247, y = 0.88933333333333,
     action = simpleButtons.defaultButtonAction
 }
 
